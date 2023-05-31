33476ea8cd05584a13c007131a681a5e32e00df7
MajidTal <MajidAlhadidi@outlook.com>
2023-05-31 12:12:45 +0400

    runs in each second

--- src/main/java/com/codeline/CRON/scheduled.java
+++ src/main/java/com/codeline/CRON/scheduled.java
@@ -1,11 +1,14 @@
 package com.codeline.CRON;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 //Created a scheduled class
 //adding a component
 @Component
-
 public class scheduled {
-
+    @Scheduled(cron = "* * * * * *") // CRON expression for running every 5 minutes
+    public void scheduled() {
+        System.out.println("Hello World");
+    }
 }
