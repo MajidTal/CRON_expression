64dc3af0be5340f55007c431e9814633a64d3f08
MajidTal <MajidAlhadidi@outlook.com>
2023-05-31 14:40:16 +0400

    runs a job every day at 2:30 PM

--- src/main/java/com/codeline/CRON/scheduled.java
+++ src/main/java/com/codeline/CRON/scheduled.java
@@ -3,11 +3,10 @@ package com.codeline.CRON;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-//Created a scheduled class
-//adding a component
+
 @Component
 public class scheduled {
-    @Scheduled(cron = "0 0 * * * *") // CRON expression for running every 5 minutes
+    @Scheduled(cron = "0 30 14 * * *")
     public void scheduled() {
         System.out.println("Hello World");
     }
