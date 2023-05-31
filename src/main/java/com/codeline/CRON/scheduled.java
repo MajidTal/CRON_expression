7b716dbcc0a2732b6789d3f69d9005aca5543312
MajidTal <MajidAlhadidi@outlook.com>
2023-05-31 14:42:26 +0400

    runs a job every Monday at 8:00 AM

--- src/main/java/com/codeline/CRON/scheduled.java
+++ src/main/java/com/codeline/CRON/scheduled.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class scheduled {
-    @Scheduled(cron = "0 30 14 * * *")
+    @Scheduled(cron = "0 0 8 ? * MON *")
     public void scheduled() {
         System.out.println("Hello World");
     }
