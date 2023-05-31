5df4fc08876b5a3dfd6b5bcf7f2726cb73c690e6
MajidTal <MajidAlhadidi@outlook.com>
2023-05-31 11:48:29 +0400

    adding a EnableScheduling in a main class

---
+++ src/main/java/com/codeline/CRON/scheduled.java
@@ -0,0 +1,7 @@
+package com.codeline.CRON;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class scheduled {
+}
---
+++ src/test/java/com/codeline/CRON/CronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeline.CRON;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
